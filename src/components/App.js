c2b684f19ed3a9db2ad73e2d5269087718468054
Rahul Rajabhoj <rahulrajabhoj@gmail.com>
2021-04-05 12:49:02 +0530

    feat: created store and connected to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
-function App() {
-  return (
-    <div className="App">
-      APP Component
-    </div>
-  );
+import React from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+
+class App extends React.Component {
+
+  componentDidMount() {
+    const { dispatch } = this.props
+
+    dispatch(handleInitialData())
+  }
+
+  render() { 
+    return (
+      <div className="App">
+        APP Component
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from '../src/components/App';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers';
+import middlewares from './middlewares';
+
+const store = createStore(reducers, middlewares)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
