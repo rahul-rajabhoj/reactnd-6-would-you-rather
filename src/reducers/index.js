cc6b310f678ce20e7f878d95a8b7257ac9b84367
Rahul Rajabhoj <rahulrajabhoj@gmail.com>
2021-04-05 13:12:26 +0530

    feat: added authedUser reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { loadingBarReducer } from 'react-redux-loading-bar'
 
 import questions from './questions'
 import users from './users'
+import authedUser from './authedUser'
 
 export default combineReducers({
     users,
     questions,
+    authedUser,
     loadingBar: loadingBarReducer,
 })
