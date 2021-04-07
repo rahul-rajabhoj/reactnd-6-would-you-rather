5fc6271827b1fad6002196a4b160d6721c46f0dd
Rahul Rajabhoj <rahulrajabhoj@gmail.com>
2021-04-08 01:11:01 +0530

    feat: Added users and questions action

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,44 @@
+import { showLoading, hideLoading } from 'react-redux-loading-bar'
+import { _saveQuestion as saveQuestion } from '../utils/_DATA'
+import { addQuestionToUser } from './users'
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const ADD_QUESTION = 'ADD_QUESTION'  
+export const ADD_ANSWER = 'ADD_ANSWER'  
 
 export function receiveQuestions(questions) {
     return {
         type: RECEIVE_QUESTIONS,
         questions
+    }
+}
+
+export function handleAddQuestion(optionOneText, optionTwoText, author) {
+    return async (dispatch) => {
+        dispatch(showLoading())
+        return saveQuestion({optionOneText, optionTwoText, author}).then((question) => {
+            dispatch(addQuestion(question))
+            dispatch(addQuestionToUser(question))
+            dispatch(hideLoading())
+        }).catch( error => {
+            console.error('Error in handleAddQuestion', error)
+            dispatch(hideLoading())
+        })
+    }
+} 
+
+function addQuestion(question) {
+    return {
+        type: ADD_QUESTION,
+        question
+    }
+}
+
+export function addAnswer(authedUser, qid, answer) {
+    return {
+        type: ADD_ANSWER,
+        authedUser, 
+        qid, 
+        answer,
     }
 }
--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,40 @@
+import { showLoading, hideLoading } from 'react-redux-loading-bar'
+import { _saveQuestionAnswer as saveQuestionAnswer} from "../utils/_DATA"
+import { addAnswer } from './questions'
+
 export const RECEIVE_USERS = 'RECEIVE_USERS'
+export const ADD_QUESTION_TO_USER = 'ADD_QUESTION_TO_USER'
+export const ADD_ANSWER_TO_USER = 'ADD_ANSWER_TO_USER'  
 
 export function receiveUsers(users) {
     return {
         type: RECEIVE_USERS,
         users
+    }
+}
+
+export function addQuestionToUser(question) {
+    return {
+        type: ADD_QUESTION_TO_USER,
+        question
+    }
+}
+
+function addAnswerToUser(authedUser, qid, answer) {
+    return {
+        type: ADD_ANSWER_TO_USER,
+        authedUser,
+        qid,
+        answer
+    }
+}
+
+export function handleSaveQuestionAnswer(authedUser, qid, answer) {
+    return async (dispatch) => {
+        dispatch(addAnswerToUser(authedUser, qid, answer))
+        dispatch(addAnswer(authedUser, qid, answer))
+        return saveQuestionAnswer({authedUser, qid, answer}).catch( error => {
+            console.error('Error in handleSaveQuestionAnswer', error)
+        })
     }
 }
