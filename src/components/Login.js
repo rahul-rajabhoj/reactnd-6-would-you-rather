b2b071dd91b0b1aec4701520f77db8fce1ec4757
Rahul Rajabhoj <rahulrajabhoj@gmail.com>
2021-04-08 04:21:52 +0530

    feat: Updated ui of login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,46 +1,70 @@
-import React from 'react'
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
+import { Segment, Grid, Header, Image, Form} from 'semantic-ui-react';
 import { login } from '../actions/authedUser'
 
 class Login extends React.Component {
 
     state = {
-        value: ''
+        userId: ''
     }
 
-    handleChange = (event) => {
-        this.setState({value: event.target.value});
+    handleChange = (e, { value }) => {
+        this.setState({ userId: value })
     }
     
     handleSubmit = (event) => {
         const { dispatch } = this.props
+        const { userId } = this.state
         event.preventDefault();
-        dispatch(login(this.state.value))
+        if(userId !== '') 
+            dispatch(login(userId))
+        else 
+            alert('Please select a user to sign in !!')
     }
 
     render() { 
-        const { value } = this.state
+        const { userId } = this.state
         const { authedUser, users } = this.props
         if(authedUser !== null) {
             return <Redirect to='/' />
         }
 
         return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                    Select User to Login:
-                        <select value={this.state.value} onChange={this.handleChange}>
-                            <option disabled value="">Select User</option>
-                            { Object.keys(users).map((id) => (
-                                <option key={id} value={id}>{users[id].name}</option>
-                            ))}
-                        </select>
-                    </label>
-                    <input type="submit" disabled={value === ''} value="Submit" />
-                </form>
-            </div>
+            <Fragment>
+                <Segment.Group>
+                    <Header as='h4' block attached='top' textAlign='center'>
+                        <Header.Content>Welcome to the Would You Rather App!</Header.Content>
+                        <Header.Subheader>Please sign in to continue</Header.Subheader>
+                    </Header>
+                    <div>
+                        <Grid padded textAlign='center'>
+                            <Grid.Row className='login-grid'>
+                                <Grid.Column width={16}>
+                                    <Image src='/images/logo192.png' centered />
+                                    <br />
+                                    <Form onSubmit={this.handleSubmit}>
+                                        <Header as='h2' color='green'>
+                                            Sign in
+                                        </Header>
+                                        <Form.Dropdown
+                                            placeholder='Select User'
+                                            fluid
+                                            selection
+                                            options={users}
+                                            value={userId}
+                                            onChange={this.handleChange}
+                                            required
+                                        />
+                                        <Form.Button content='Sign in' positive fluid />
+                                    </Form>
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </div>
+                </Segment.Group>
+            </Fragment>
         );
     }
 }
@@ -48,7 +72,12 @@ class Login extends React.Component {
 function mapStateToProps({ authedUser, users }) {
     return {
         authedUser,
-        users
+        users: Object.keys(users).map(id => ({
+            key: id,
+            text: users[id].name,
+            value: id,
+            image: { avatar: true, src: users[id].avatarURL }
+        }))
     }
 }
 
