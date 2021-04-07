3ffb17c796379ddc92a333b12ad1d8c85d66192e
Rahul Rajabhoj <rahulrajabhoj@gmail.com>
2021-04-07 11:54:19 +0530

    feat: Added logic to fetch leaders data from store

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -3,12 +3,36 @@ import { connect } from 'react-redux'
 
 class LeaderBoard extends React.Component {
     render() { 
+        const { leaders } = this.props
         return (
             <div>
-                LeaderBoard Component
+                {leaders.map((leader) => (
+                    <div key={leader.id}>
+                        <img src={leader.avatarURL} alt={leader.name}/>
+                        <p>Name: {leader.name}</p>
+                        <p>Answered Question: {leader.answeredQuestion}</p>
+                        <p>Created Question: {leader.createdQuestion}</p>
+                        <p>Score: {leader.score}</p>
+                    </div>
+                ))}
             </div>
-        );
+        )
     }
 }
 
-export default connect()(LeaderBoard);
+function mapStateToProps({ users }) {
+    const leaders = Object.values(users)
+        .map((user) => ({
+            ...user,
+            answeredQuestion: Object.values(user.answers).length,
+            createdQuestion: user.questions.length,
+            score: Object.values(user.answers).length + user.questions.length
+        }))
+        .sort((a,b) => b.score - a.score)
+
+    return {
+        leaders
+    }
+}
+
+export default connect(mapStateToProps)(LeaderBoard);
