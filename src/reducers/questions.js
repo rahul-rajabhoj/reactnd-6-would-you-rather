256cbe85ecf1543f3722758453a606cde704c5b3
Rahul Rajabhoj <rahulrajabhoj@gmail.com>
2021-04-08 01:16:42 +0530

    feat: Updated users and questions reducers

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,9 +1,29 @@
-import { RECEIVE_QUESTIONS } from "../actions/questions";
+import { RECEIVE_QUESTIONS, ADD_ANSWER, ADD_QUESTION } from "../actions/questions";
 
 export default function questions(state = {}, action) {
     switch(action.type) {
         case RECEIVE_QUESTIONS: 
             return action.questions
+
+        case ADD_QUESTION:
+            const { question } = action;
+            return {
+                ...state,
+                [question.id]: question
+            }
+        
+        case ADD_ANSWER:
+            const { authedUser, qid, answer } = action; 
+            return {
+                ...state,
+                [qid]: {
+                    ...state[qid],
+                    [answer]: {
+                        ...state[qid][answer],
+                        votes: state[qid][answer].votes.concat(authedUser)
+                    }
+                }
+            }
         
         default:
             return state
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,9 +1,32 @@
-import { RECEIVE_USERS } from "../actions/users";
+import { RECEIVE_USERS, ADD_ANSWER_TO_USER, ADD_QUESTION_TO_USER } from "../actions/users";
 
 export default function users(state = {}, action) {
     switch(action.type) {
         case RECEIVE_USERS: 
             return action.users
+
+        case ADD_QUESTION_TO_USER: 
+            const { question } = action
+            return {
+                ...state,
+                [question.author]: {
+                    ...state[question.author],
+                    questions: state[question.author].questions.concat(question.id)
+                }
+            }
+
+        case ADD_ANSWER_TO_USER: 
+            const { authedUser, qid, answer } = action
+            return {
+                ...state,
+                [authedUser]: {
+                    ...state[authedUser],
+                    answers: {
+                        ...state[authedUser].answers,
+                        [qid]: answer
+                    }
+                }
+            }
         
         default:
             return state
